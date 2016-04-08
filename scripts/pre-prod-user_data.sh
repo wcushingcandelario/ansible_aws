81e3361d67faa5b3023aa6791118a34ebc7859c2
root <root@ip-10-33-232-215.eu-west-1.compute.internal>
2016-04-08 15:18:58 +0000

    updated config management

--- scripts/pre-prod-user_data.sh
+++ scripts/pre-prod-user_data.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 export ENV=pre-prod
+
+mv /opt/ovc/Platform-Dynamic-Objects/config/unifiedConfig.properties /opt/ovc/Platform-Dynamic-Objects/config/unifiedConfig.properties.ORIG
+
+mv /opt/ovc/Platform-Dynamic-Objects/config/unifiedConfig.properties.$ENV /opt/ovc/Platform-Dynamic-Objects/config/unifiedConfig.properties
+
+
+mv /opt/ovc/Platform-Dynamic-Objects/config/tools.properties /opt/ovc/Platform-Dynamic-Objects/config/tools.properties.ORIG
+
+mv /opt/ovc/Platform-Dynamic-Objects/config/tools.properties.$ENV /opt/ovc/Platform-Dynamic-Objects/config/tools.properties
+
+
+/etc/init.d/jetty restart
