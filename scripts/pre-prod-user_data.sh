71688c2d70ed7def1625a08a445b5eb2d93f6048
Robert Small <robert.small@oneviewcommerce.com>
2016-06-07 22:53:53 +0100

    DEVOPS-130 Added dashboard files to User Data script

--- scripts/pre-prod-user_data.sh
+++ scripts/pre-prod-user_data.sh
@@ -11,5 +11,13 @@ export FILE_NAME=/opt/ovc/Platform-Dynamic-Objects/config/tools.properties
 cp $FILE_NAME $FILE_NAME.ORIG
 sudo mv $FILE_NAME.$ENV $FILE_NAME
 
+export FILE_NAME=/var/www/html/ovcdashboard/app/Config/database.php
+cp $FILE_NAME $FILE_NAME.ORIG
+sudo mv $FILE_NAME.$ENV $FILE_NAME
+
+export FILE_NAME=/var/www/html/ovcdashboard/app/Config/setting_var.php
+cp $FILE_NAME $FILE_NAME.ORIG
+sudo mv $FILE_NAME.$ENV $FILE_NAME
 
 sudo /etc/init.d/jetty restart
+sudo /etc/init.d/httpd restart
