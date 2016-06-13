1be3d1305a4fddc7e04278d2fca982d7b93ab6ab
Robert Small <robert.small@oneviewcommerce.com>
2016-06-13 14:15:02 +0100

    DEVOPS-136 Add patching into user data scripts

--- scripts/prod-user_data.sh
+++ scripts/prod-user_data.sh
@@ -24,5 +24,9 @@ export FILE_NAME=/var/www/html/ovcdashboard/app/Config/setting_var.php
 cp $FILE_NAME $FILE_NAME.ORIG
 sudo mv $FILE_NAME.$ENV $FILE_NAME
 
+export FILE_NAME=/opt/ovc/posWebapp/WEB-INF/web.xml
+sudo /usr/bin/patch $FILE_NAME $FILE_NAME.patch.$ENV
+
+
 sudo /sbin/service jetty restart
 sudo /sbin/service httpd restart
