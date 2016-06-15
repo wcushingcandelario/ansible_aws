d045163fbfbd2d6b8df2e2e49c6a50a21eee4191
Bill Cushing-Candelario <bill@ip-10-33-232-215.eu-west-1.compute.internal>
2016-06-15 13:50:19 +0000

    modified property files, deleted two bash scripts for preprod, modified nginx, modfied defaults for oneview to include new rds endpoint

---
+++ scripts/preprod-user_data.sh
@@ -0,0 +1,28 @@
+#!/bin/bash#
+
+exec 1>/root/userdata.log 2>&1
+set -x
+
+/bin/date
+
+export ENV=pre-prod
+
+
+export FILE_NAME=/opt/ovc/Platform-Dynamic-Objects/config/unifiedConfig.properties
+cp $FILE_NAME $FILE_NAME.ORIG
+sudo mv $FILE_NAME.$ENV $FILE_NAME
+
+export FILE_NAME=/opt/ovc/Platform-Dynamic-Objects/config/tools.properties
+cp $FILE_NAME $FILE_NAME.ORIG
+sudo mv $FILE_NAME.$ENV $FILE_NAME
+
+export FILE_NAME=/var/www/html/ovcdashboard/app/Config/database.php
+cp $FILE_NAME $FILE_NAME.ORIG
+sudo mv $FILE_NAME.$ENV $FILE_NAME
+
+export FILE_NAME=/var/www/html/ovcdashboard/app/Config/setting_var.php
+cp $FILE_NAME $FILE_NAME.ORIG
+sudo mv $FILE_NAME.$ENV $FILE_NAME
+
+sudo /sbin/service jetty restart
+sudo /sbin/service httpd restart
