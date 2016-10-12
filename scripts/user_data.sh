73786f82d6754bb0b594bfb4e92927cb18a52935
Bill Candelario <bill@oneviewcommerce.com>
2016-10-12 09:56:08 -0400

    Filebeat.yml and Logstash-Forwarder.crt to be exported
    Updated to include  filebeat.yml and logstash-forwarder.crt.  This files have been modified with the .preprod and .prod extensive.

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -34,6 +34,14 @@ export FILE_NAME=/var/www/html/ovcdashboard/app/Config/setting_var.php
 cp $FILE_NAME $FILE_NAME.ORIG
 sudo mv $FILE_NAME.$ENV $FILE_NAME
 
+export FILE_NAME=/etc/filebeat/filebeat.yml
+cp $FILE_NAME $FILE_NAME.ORIG
+sudo mv $FILE_NAME.$ENV $FILE_NAME
+
+export FILE_NAME=/etc/pki/tls/certs/logstash-forwarder.crt
+cp $FILE_NAME $FILE_NAME.ORIG
+sudo mv $FILE_NAME.$ENV $FILE_NAME
+
 export FILE_NAME=/opt/ovc/posWebapp/WEB-INF/web.xml
 export LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 sudo /usr/bin/patch $FILE_NAME $FILE_NAME.patch.$ENV
