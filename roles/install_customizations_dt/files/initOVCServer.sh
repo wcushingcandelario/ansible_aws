1f560417f58e205aa8bbb33472e76f46fb00fb4e
Bill Candelario <bill@oneviewcommerce.com>
2017-04-28 13:37:36 -0400

    Removed dt customizations

--- roles/install_customizations_dt/files/initOVCServer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Dovc.config.dir="/opt/ovc/Platform-Dynamic-Objects/config" -cp "/opt/ovc/lib/*":"/opt/ovc/lib/POSMClient-Tools-DT.jar":"/opt/ovc/lib/ext/*" com.oneview.tools.posmclient.UnifiedImporterAndSyncUpdater "/opt/ovc/Platform-Dynamic-Objects/config/tools.properties" "/opt/ovc/Base-Location-Import-Files" "/opt/ovc/Application-Dynamic-Objects" eComInit
